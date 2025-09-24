675aa5742f2a005c571007ede71c92ebc32cf12d
Tyler Steppat <tksteppat@csuchico.edu>
2025-09-24 11:15:07 -0700

    Terminates after typing nothing, closes #1

--- main.cpp
+++ main.cpp
@@ -19,14 +19,18 @@ int main(){
   pick = rand() % 4;
   cout << "What are you listening to?\n";
   getline(cin,input);
+  if (input == "nothing")
+      return 0;
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
-  do{
+  while( input != "nothing" ) {
     cout << "What's next?\n";
     getline(cin,input);
+    if (input == "nothing")
+      return 0;
     pick = rand() % 4;
     cout << VALIDATION[pick] << "!\n";
-  }while( input != "nothing" );
+  }
 
   return 0;
 }
